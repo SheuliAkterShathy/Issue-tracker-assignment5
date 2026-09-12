3b86d9e0a398e67f2f27b6fcfd920e80312b12e8
Sheuli Akter <sheuliakter85@gmail.com>
2026-09-12 22:18:24 +0600

    display card done

--- script/issues.js
+++ script/issues.js
@@ -5,44 +5,47 @@ const allIssuesBtn = document.getElementById("allIssuesBtn");
 
 // })
 
-async function loadIssues(){
-      const res = await fetch(
+const createElements = (arr) => {
+  const htmlElements = arr.map((el) => `<button class ="mr-2 px-2 py-1 rounded-lg ${el==="bug"? "bg-red-400":el === "help wanted"? "bg-orange-300":"bg-green-400"}">${el}<button/>`);
+  return htmlElements.join(" ");
+};
+
+async function loadIssues() {
+  const res = await fetch(
     `https://phi-lab-server.vercel.app/api/v1/lab/issues`,
   );
   const data = await res.json();
   displayIssues(data.data);
 }
-function displayIssues (issues){
- issues.forEach(issue => {
-
+function displayIssues(issues) {
+  issues.forEach((issue) => {
     const div = document.createElement("div");
     div.innerHTML = `
     
-     <div class="bg-white shadow p-4 rounded-md h-full">
+     <div class="bg-white shadow p-4 rounded-md card h-full">
             <div class="flex justify-between">
-              <img class="w-6 h-6" src="./assets/Open-Status.png" alt="" />
-              <button class="btn">high</button>
+              <img class="w-6 h-6"    src="${issue.status === "open" ? "./assets/Open-Status.png" : "./assets/Closed-Status.png"}" alt="" />
+              <button class="btn">${issue.priority}</button>
             </div>
-            <h2 class="font-semibold">${issue.title}</h2>
+            <h2 class="font-semibold h-12">${issue.title}</h2>
             <p class="my-2 line-clamp-2">${issue.description}</p>
-            <div class="badge badge-success">
-
-              Success
-            </div>
-            <div class="badge badge-warning">
-
-              Warning
+           
+            <div>
+             ${createElements(issue.labels)}
+              
             </div>
             
+           
+            
             <hr class="my-4 border-gray-400">
-            <p>by jon doe</p>
-            <p>1/1/23</p>
+            <p># ${issue.author}</p>
+            <p>${issue.createdAt}</p>
           </div>
     
     
-    `
+    `;
     IssuesContainer.appendChild(div);
- });
+  });
 }
-loadIssues()
+loadIssues();
 // displayIssues()
